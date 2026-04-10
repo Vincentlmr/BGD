ff11725d1c1355f901f9a6cdd003677127ee3457
Vincentlmr <vincelm95@orange.fr>
2026-04-10 10:47:22 +0200

    Add files via upload

---
+++ graphe.R
@@ -0,0 +1,14 @@
+library(igraph)
+help("igraph")
+
+
+library(readr)
+paths_finished_nettoye <- read_csv("paths_finished_nettoye.csv", 
+                                   col_names = FALSE)
+
+
+
+
+
+
+read_graph("IS4/S8/BGD/wikispeedia_paths-and-graph/paths_finished_nettoye.csv")
